d022b25cfa6d2fbcd5c29dac7709fc1f921fa473
Tanjim Ahmed Chowdhury <155260197+tanjimN4@users.noreply.github.com>
2025-05-27 14:44:38 +0600

    Update firebase.js

--- Client/firebase.js
+++ Client/firebase.js
@@ -1,17 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBOZWhdwXD_bjzVAMeAexMvWpcy05Wke1c",
-  authDomain: "user-email-password-auth-13c4c.firebaseapp.com",
-  projectId: "user-email-password-auth-13c4c",
-  storageBucket: "user-email-password-auth-13c4c.firebasestorage.app",
-  messagingSenderId: "669810493327",
-  appId: "1:669810493327:web:492ef5a5ddf76130816dfa"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
